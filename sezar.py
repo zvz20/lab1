de89b27b64ef9421f0171a4fd2513e90ddd627c1
zvz20 <57296118+zvz20@users.noreply.github.com>
2019-11-07 18:13:50 +0200

    Update sezar.py

--- sezar.py
+++ sezar.py
@@ -1,14 +1,17 @@
 alph = ("abcdefghijklmnopqrstuvwsyzabcdefghijklmnopqrstuvwsyz")
+alph2 = ("абвгґдеєжзиіїйклмнопрстуфхцчшщьюя")
 num=("1234567890")
 encr = input("Enter a massage: ")
 encr = encr+" "
-key = int(input("Enter a key: "))
+key = 1
 encr = encr.lower()
 encr2 = ""
 a=""
 for letter in encr:
     position = alph.find(letter)
     pos = num.find(letter)
+    pos2 = alph2.find(letter)
+    newPosititon3 = pos2 + key
     newPosititon2 = pos + key
     newPosititon = position + key
     if letter in alph:
@@ -20,6 +23,15 @@ for letter in encr:
             a=""
 
         encr2 = encr2 + alph[newPosititon]
+    elif letter in alph2:
+        if a != "":
+            a=int(a)
+            a=a+key
+            a=str(a)
+            encr2=encr2+a
+            a=""
+
+        encr2 = encr2 + alph2[newPosititon3]
     elif letter in num:
         a=a+letter
     else:
